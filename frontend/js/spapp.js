efb3202cb9b3d06b75d4cc2eb4eaf2476f99bc6e
dzelilatin <dzelila.tinjak@stu.ibu.edu.ba>
2025-06-07 14:11:33 +0200

    Update frontend JavaScript files

--- frontend/js/spapp.js
+++ frontend/js/spapp.js
@@ -1,14 +1,4 @@
 $(document).ready(function() {
-<<<<<<< HEAD
-    var app = $.spapp({
-      defaultView: "home",
-      templateDir: "./tpl/",
-      pageNotFound: "error_40324",
-    });
-  
-    app.run();
-  });
-=======
   var app = $.spapp({
     defaultView: "home",
     templateDir: "",  // Keep it empty to prevent double tpl/
@@ -17,4 +7,3 @@ $(document).ready(function() {
 
   app.run();
 });
->>>>>>> 2aa61c51e6c4711150514c4f9f0942cd7d323889
